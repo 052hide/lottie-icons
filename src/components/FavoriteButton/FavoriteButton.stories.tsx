ebf452ddc3909d91975801e39de40200601f38bd
052hide <hidetoshi1990@gmail.com>
2022-03-19 14:07:44 +0900

    create component wrapper

--- src/components/FavoriteButton/FavoriteButton.stories.tsx
+++ src/components/FavoriteButton/FavoriteButton.stories.tsx
@@ -1,16 +1,22 @@
 import { ComponentMeta, ComponentStoryObj } from '@storybook/react'
+import { useState } from 'react'
 
 import { FavoriteButton } from './FavoriteButton'
 
-const meta: ComponentMeta<typeof FavoriteButton> = {
+const ComponentWithState = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+  return <FavoriteButton isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
+}
+
+const meta: ComponentMeta<typeof ComponentWithState> = {
   title: 'FavoriteButton',
-  component: FavoriteButton,
+  component: ComponentWithState,
   parameters: {
     controls: { expanded: true },
   },
 }
 export default meta
 
-export const Base: ComponentStoryObj<typeof FavoriteButton> = {
+export const Base: ComponentStoryObj<typeof ComponentWithState> = {
   args: {},
 }
